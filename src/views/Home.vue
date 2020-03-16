c6d8becf8bb9912ee64cff4f4cafd475e37c5e5c
NatnaelBerhanu-1 <natnaelberhanu18@gmail.com>
2020-03-16 13:58:30 +0300

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <b-container class="bv-example-row bg-primary">
+      <b-button variant="danger">
+        this is button
+      </b-button>
+      <b-row>
+        <b-col>
+          <h1 v-for="i in 10" :key="i">this is test</h1>
+        </b-col>
+        <b-col>2 of 3</b-col>
+        <b-col>3 of 3</b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld
+  }
+};
+</script>
